894f39dae779623e2d6d1c969bd9f300d862f7d9
wvqusrai <1250837680@qq.com>
2019-09-14 20:16:13 +0800

    Delete geo_wvqusrai_demo.js

--- geo_wvqusrai_demo.js
+++
@@ -1,4 +0,0 @@
-var ggbApp = new GGBApplet({ "appName": "graphing", "width": 800, "height": 600, "showToolBar": true, "showAlgebraInput": true, "showMenuBar": true }, true);
-window.addEventListener("load", function() {
-    ggbApp.inject('ggb-element');
-});
